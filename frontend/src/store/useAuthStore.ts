e95e1238336c71f535da9a4ed5b50072360c755f
kingsedu <chinedueh26@yahoo.com>
2025-10-04 23:45:01 +0100

    updated the front-end

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,6 +1,16 @@
 import { create } from "zustand";
 
-export const useAuthStore = create((set) => ({
+interface AuthUser {
+  name: string;
+  _id: number;
+}
+interface AuthState {
+  authUser: AuthUser | null;
+  isLoading: boolean;
+  isLoggedIn: boolean;
+  login: () => void;
+}
+export const useAuthStore = create<AuthState>((set) => ({
   authUser: { name: "john", _id: 123 },
   isLoading: false,
   isLoggedIn: false,
