d58d76250de9aeee4b1f4b1f5acc3beacd596fb3
kingsedu <chinedueh26@yahoo.com>
2025-10-08 11:03:16 +0100

    made some addups inn the sign-up page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,49 @@
 // import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./pages/SignUp";
 import ChatPage from "./pages/ChatPage";
 import Login from "./pages/Login";
-import { useAuthStore } from "./store/useAuthStore";
+import { useAuthUser } from "./store/useAuthStore";
+import { useEffect } from "react";
+import PageLoader from "./components/PageLoader";
+import { Toaster } from "react-hot-toast";
 export default function App() {
-  const { authUser, isLoading, login, isLoggedIn } = useAuthStore();
-  console.log(authUser, "auth user object");
+  // const { authUser, isLoading, login, isLoggedIn } = useAuthStore();
+  const { checkAuth, isCheckingAuth, authUserMain } = useAuthUser();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  console.log(authUserMain);
+  /* console.log(authUser, "auth user object");
   console.log(isLoading, "is loading");
   console.log(login, "login");
-  console.log(isLoggedIn, "is logged in always");
+  console.log(isLoggedIn, "is logged in always"); */
+
+  if (isCheckingAuth) return <PageLoader />;
 
   return (
-    <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
+    <div className="relative flex items-center justify-center min-h-screen p-4 overflow-hidden bg-slate-900">
       {/* Decorators - grid and glow shapes */}
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
-      <button onClick={login} className="z-10">
-        loginbutton
-      </button>
+
       <Routes>
-        <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/"
+          element={authUserMain ? <ChatPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={!authUserMain ? <Login /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUserMain ? <SignUp /> : <Navigate to={"/"} />}
+        />
       </Routes>
+      <Toaster />
     </div>
   );
 }
---
+++ frontend/src/components/BorderAnimatedContainer.tsx
@@ -0,0 +1,16 @@
+// How to make animated gradient border 👇
+// https://cruip-tutorials.vercel.app/animated-gradient-border/
+
+import React, { ReactElement } from "react";
+
+export default function BorderAnimatedContainer({
+  children,
+}: {
+  children: ReactElement;
+}) {
+  return (
+    <div className="w-full h-full [background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.cyan.500)_86%,_theme(colors.cyan.300)_90%,_theme(colors.cyan.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-2xl border border-transparent animate-border  flex overflow-hidden">
+      {children}
+    </div>
+  );
+}
