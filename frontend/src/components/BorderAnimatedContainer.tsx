0197b42247d8a9cbac0ca29b9c7a1fa072762e15
kingsedu <chinedueh26@yahoo.com>
2025-10-08 12:33:42 +0100

    made updates based on the deploy error

--- frontend/src/components/BorderAnimatedContainer.tsx
+++ frontend/src/components/BorderAnimatedContainer.tsx
@@ -1,13 +1,11 @@
 // How to make animated gradient border 👇
 // https://cruip-tutorials.vercel.app/animated-gradient-border/
+import type { ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+}
 
-import React, { ReactElement } from "react";
-
-export default function BorderAnimatedContainer({
-  children,
-}: {
-  children: ReactElement;
-}) {
+export default function BorderAnimatedContainer({ children }: Props) {
   return (
     <div className="w-full h-full [background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.cyan.500)_86%,_theme(colors.cyan.300)_90%,_theme(colors.cyan.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-2xl border border-transparent animate-border  flex overflow-hidden">
       {children}
